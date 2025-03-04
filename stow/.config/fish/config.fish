0cff6c90a723109123bcf090a9c69c480fea1e11
whizzkid <tharunmandava@gmail.com>
2025-03-04 17:16:02 +0000

    changed terminal prompt, and removed vi indicators in prompt

--- stow/.config/fish/config.fish
+++ stow/.config/fish/config.fish
@@ -1,6 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fish_vi_key_bindings
+    set fish_key_bindings fish_vi_key_bindings
+    function fish_mode_prompt
+        #Do Nothing(overrides default behavior)
+    end
+
+    echo (prompt_pwd) (fish_git_prompt)
 
     #alias
     alias work="cd /mnt/d/work/"
